abf4d1851cc40abef5eb148fbdc1f5a9d20763c2
Marieke <69918471+Makie2020@users.noreply.github.com>
2022-11-21 10:51:15 +0100

    Percentage Scroller

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ function buttonClick() {
   let responsive_class_name = 'responsive'
   navbar.classList.toggle(responsive_class_name)
 }
+const updateScrollPercentage = function() { 
+  const heightOfWindow = window.innerHeight,
+    contentScrolled = window.pageYOffset,
+    bodyHeight = document.body.offsetHeight,
+    percentage = document.querySelector("[data-scrollPercentage] .percentage")
+    percentageVal = document.querySelector("#percentage-value")
+
+  if(bodyHeight - contentScrolled <= heightOfWindow) {
+    percentage.style.width = "100%"
+  }
+  else {
+    const total = bodyHeight - heightOfWindow,
+      got = contentScrolled,
+      percent = parseInt((got/total) * 100)
+    percentage.style.width = percent + "%"
+  }
+}
+
+window.addEventListener('scroll', updateScrollPercentage)
