4ebd2371796e1a7651b7a5bfb5b047122dfb1fc0
Marieke <69918471+Makie2020@users.noreply.github.com>
2022-11-24 11:25:32 +0100

    Slider prev fixed

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,32 +1,31 @@
-class slider{
-  constructor(){
-      this.slide = [
-        "./images/Slider/image 1.jpg",
-        "./images/Slider/image 2.jpg",
-        "./images/Slider/image 3.jpg",
-        "./images/Slider/image 4.jpg",
-        "./images/Slider/image 5.jpg",
-        "./images/Slider/image 6.jpg",];
-      this.next = document.getElementById('next');
-      this.previous = document.getElementById('previous');
-      this.index = 1;
-      this.next.onclick = () => {
-          if (this.index >= 6){
-              this.index = 0;
-          }
-          document.getElementById("slider__image").src = this.slide[this.index];
-          this.index++;
-          console.log(this.index);
+class slider {
+  constructor() {
+    this.slide = [
+      "./images/Slider/image 1.jpg",
+      "./images/Slider/image 2.jpg",
+      "./images/Slider/image 3.jpg",
+      "./images/Slider/image 4.jpg",
+      "./images/Slider/image 5.jpg",
+      "./images/Slider/image 6.jpg",];
+    this.next = document.getElementById('next');
+    this.previous = document.getElementById('previous');
+    this.index = 0;
+    this.next.onclick = () => {
+      this.index++;
+      if (this.index > 5)  {
+        this.index = 0;
       }
-      this.previous.onclick = () => {
-        document.getElementById("slider__image").src = this.slide[this.index];
-        console.log(this.slide[this.index]);
-        if (this.index < 1){
-            this.index = 6;
-        }
-        this.index--;
-        console.log(this.slide[this.index]);
-        console.log(this.index)
+      document.getElementById("slider__image").src = this.slide[this.index];
+      console.log(this.index);
+    };
+    this.previous.onclick = () => {
+      this.index--;
+      console.log(this.index);
+      if (this.index < 0) {
+        this.index = 5;
+      }
+      console.log(this.index);
+      document.getElementById("slider__image").src = this.slide[this.index];
     }
   }
 };
